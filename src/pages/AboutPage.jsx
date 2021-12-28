0c3c0d66f2e591c7eba7c6634b4015892d0c5611
nosnetrom <nosnetrom@gmail.com>
2021-12-28 14:42:18 -0600

    Removed unused elements.

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import Card from '../components/shared/Card'
 
 function AboutPage(props) {
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import { FaEnvelope } from 'react-icons/fa';
 import Card from '../components/shared/Card'
 
