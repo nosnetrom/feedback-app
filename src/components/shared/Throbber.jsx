068bae8612bdd6338897717fa0786beb1cdeb32a
nosnetrom <nosnetrom@gmail.com>
2022-01-02 12:50:44 -0600

    Adding db.json, throbber assets, dev script to package.json

---
+++ src/components/shared/Throbber.jsx
@@ -0,0 +1,9 @@
+import throbber from '../assets/throbber.gif'
+
+function Throbber() {
+  return (
+    <img src={throbber} alt="Loading..." style={{width: "193px", height: "192px", margin: "auto", display: "block"}} />
+  )
+}
+
+export default Throbber
